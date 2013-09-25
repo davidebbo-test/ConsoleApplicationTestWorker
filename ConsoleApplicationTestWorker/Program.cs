c740d27d2de7686601d313b13546ef49ed747ad3
davidebbo <david.ebbo@microsoft.com>
2013-09-25 13:31:23 -0700

    Initial commit

---
+++ ConsoleApplicationTestWorker/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationTestWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; ; i++)
+            {
+                var client = new HttpClient();
+                var response = client.GetAsync(String.Format("http://davidebbo.com/abcd/{0}", i)).Result;
+                Console.WriteLine(response.StatusCode);
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
